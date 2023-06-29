153d726a313765dd98794432739415ab5f2573b5
xnclxud9 <xnclxud9@gmail.com>
2023-06-29 20:40:34 +0900

    portfolio

--- script/project.js
+++ script/project.js
@@ -1,21 +1,9 @@
 // 슬라이드
-// const swiper = new Swiper(".mySwiper", {
-//   slidesPerView: "1",
-//   spaceBetween: 0,
-//   loop: true,
-//   autoHeight: true, slideToClickedSlide: true,
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-// });
-
 $('.project_wrap').slick({
   slidesToShow: 1,
   adaptiveHeight: true,
   prevArrow: "<button type='button' class='slick-prev'><i class='fas fa-chevron-left'></i> Prev</button>",
   nextArrow: "<button type='button' class='slick-next'>Next <i class='fas fa-chevron-right'></i></button>",
-  // appendArrows: ,
   asNavFor: '.slide_nav',
 });
 
